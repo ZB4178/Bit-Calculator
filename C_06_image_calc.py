c7b9877a9a7837463bec12ae36302a56f61e6fd4
ZB4178 <brockelsbyz4178@masseyhigh.school.nz>
2025-10-16 10:50:20 +1300

    Add files via upload

---
+++ C_06_image_calc.py
@@ -0,0 +1,40 @@
+# Ask user for width and loop until they
+# enter a number that is more than zero
+def int_check(question, low):
+
+    error = f"Please enter a number that is more than or equal to {low}\n"
+    while True:
+
+        try:
+            #Ask user for number
+            response = int(input(question))
+
+            #Check number is more than zero
+            if response >= low:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
+#calculates how many bits are needed to represent an integer
+def image_calc():
+    # get the image dimensions
+    width = int_check("width: ", low=1)
+    height = int_check("Height: ", low=1)
+
+     # calculate the number of pixels and multiply by 24 to get the number of bits
+    num_pixels = width * height
+    num_bits= num_pixels * 24
+
+    # setup answer and return it
+    answer = (f"Number of pixels: {width} x {height} = {num_pixels} "
+              f"\nNumber of bits: {num_pixels} x 24 = {num_bits}")
+
+    return answer
+
+#main routine goes here
+image_ans = image_calc()
+print(image_ans)
